8f4bb92ef26f78b42a9579454c13f6f97bd5e21a
adrian-dragos <adrian123795@gmail.com>
2022-12-16 11:24:38 +0200

    save working state

--- Alarm/app/src/main/java/com/example/alarm/view/AlarmActivity.kt
+++ Alarm/app/src/main/java/com/example/alarm/view/AlarmActivity.kt
@@ -1,26 +1,34 @@
 package com.example.alarm.view
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
+
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
+import com.example.alarm.AlarmHomeActivity
 import com.example.alarm.R
-import com.example.alarm.database.AlarmStore
-import com.example.alarm.database.RoomDatabase
-import com.example.alarm.utils.AlarmReceiver
-import com.example.alarm.view_model.HomeViewModel
-import com.example.alarm.view_model.HomeViewModelFactory
+import kotlinx.android.synthetic.main.alarm.*
 import kotlinx.android.synthetic.main.main_activity_view.*
-import java.util.*
 
 class AlarmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.alarm)
+
+        var alarmSound = MediaPlayer.create(this, R.raw.sound)
+        alarmSound.start()
+
+        cancel_alarm_button.setOnClickListener { cancelAlarm(alarmSound) }
+    }
+
+    private fun cancelAlarm(alarmSound: MediaPlayer) {
+        Log.d("TAG", "ajunge aici")
+        alarmSound.stop()
+        Log.d("TAG", "lalala")
+
+        val intent = Intent(this, QRActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ Alarm/app/src/main/java/com/example/alarm/view/QRActivity.kt
@@ -0,0 +1,15 @@
+package com.example.alarm.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.alarm.R
+
+class QRActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.qr)
+
+
+    }
+}
